c6e0c182df07b8cd956891bb7276a7b0c4b1805f
Adam Plesca <116078864+adamplesca@users.noreply.github.com>
2023-04-20 01:01:03 +0100

    Add files via upload

---
+++ HackerRank/Loops1.java
@@ -0,0 +1,31 @@
+// HackerRank Java Loops 1 
+// Adam Plesca
+// 20/4/23
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Loops1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        int result;
+        for(int i = 1; i <= 10; i++){
+           result = (N * i);
+           System.out.println(N+" x "+i+" = "+result);
+        }
+       
+        bufferedReader.close();
+    }
+}
